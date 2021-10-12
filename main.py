f97e596585bd80856ee13ff99f32c2ef95d33be8
qpy7777777 <1741259627@qq.com>
2021-10-12 20:19:06 +0800

    第四次提交

--- main.py
+++ main.py
@@ -16,19 +16,19 @@ if __name__=="__main__":
         file_name = os.path.join(path, file[i])
         audio, fs = librosa.load(file_name, sr=None)
         print(file_name)
-        # # 可视化mel特征 一阶mel，二阶mel特征谱图
-        # audioData_Augment.show_melspectrogram(audio,fs)
-        # # 时间频率掩码 通过同时随机去除几行(帧)和几列(频率)来实现数据的增强
-        # audioData_Augment.show_time_frequeny_mask(audio,fs)
-        # # 在原始信号上添加固定信噪比的高斯白噪,及均值为0，标准差为1的高斯白噪声
-        # audioData_Augment.show_Addnoise_waveplot(audio, fs)
-        # # 频谱，相位谱特征图
-        # audioData_Augment.show_spec_log_spectrogram(audio,fs)
-        # # 时间拉伸 Time Stretching 参数范围为[0.8,1.2]
-        # audioData_Augment.show_time_stretcheding(audio,fs)
-        # # 增加或减少音频信号的音调，同时保持持续时间不变。参数范围为[-5,5]
-        # audioData_Augment.show_pitchShift(audio,fs)
-        # # 波形位移 TimeShift
-        # audioData_Augment.show_timeShift(audio,fs)
+        # 可视化mel特征 一阶mel，二阶mel特征谱图
+        audioData_Augment.show_melspectrogram(audio,fs)
+        # 时间频率掩码 通过同时随机去除几行(帧)和几列(频率)来实现数据的增强
+        audioData_Augment.show_time_frequeny_mask(audio,fs)
+        # 在原始信号上添加固定信噪比的高斯白噪,及均值为0，标准差为1的高斯白噪声
+        audioData_Augment.show_Addnoise_waveplot(audio, fs)
+        # 频谱，相位谱特征图
+        audioData_Augment.show_spec_log_spectrogram(audio,fs)
+        # 时间拉伸 Time Stretching 参数范围为[0.8,1.2]
+        audioData_Augment.show_time_stretcheding(audio,fs)
+        # 增加或减少音频信号的音调，同时保持持续时间不变。参数范围为[-5,5]
+        audioData_Augment.show_pitchShift(audio,fs)
+        # 波形位移 TimeShift
+        audioData_Augment.show_timeShift(audio,fs)
         # 加混响
         audioData_Augment.add_reverberation(path,new_path,file_name,audio,fs,i)
